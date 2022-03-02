04785676ecb684215d6527544283415955f1a9c3
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-03-02 19:15:01 +0000

    initial examples in chap 02 done

--- samples/Ch02_where_code_runs/fig_2_9_host_selector.cpp
+++ samples/Ch02_where_code_runs/fig_2_9_host_selector.cpp
@@ -1,20 +1,19 @@
-// Copyright (C) 2020 Intel Corporation
-
-// SPDX-License-Identifier: MIT
-
 #include <CL/sycl.hpp>
 #include <iostream>
 using namespace sycl;
 
-int main() {
-  // Create queue to use the host device explicitly
-  queue Q{ host_selector{} };
-
-  std::cout << "Selected device: " <<
-    Q.get_device().get_info<info::device::name>() << "\n";
-  std::cout << " -> Device vendor: " <<
-    Q.get_device().get_info<info::device::vendor>() << "\n";
-
-  return 0;
+void print_device_info(const sycl::queue& Q) {
+    std::cout << "Selected device: " << Q.get_device().get_info<info::device::name>() << "\n";
+    std::cout << "Device vendor: " << Q.get_device().get_info<info::device::vendor>() << "\n";
+    std::cout << '\n';
 }
 
+int main() {
+    queue Q1 { host_selector {} };  // Create queue to use the host device explicitly
+    queue Q2;                       // implicit device selection (might be the host device)
+
+    print_device_info(Q1);
+    print_device_info(Q2);
+
+    return 0;
+}
