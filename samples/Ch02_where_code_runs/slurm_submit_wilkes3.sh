17d744d86815d6214e1839e64fd698e9b8fb829d
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-03-02 20:05:01 +0000

    adds gpu job submission script on wilkes3

--- samples/Ch02_where_code_runs/slurm_submit_wilkes3.sh
+++ samples/Ch02_where_code_runs/slurm_submit_wilkes3.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#SBATCH -J cpu_sycl_book_example
-#SBATCH -A DIRAC-DR004-CPU
-#SBATCH -p icelake
+#! SLURM job script for Wilkes3 (AMD EPYC 7763, ConnectX-6, A100)
+
+#SBATCH -J gpu_wilkes3_sycl_book_example
+#SBATCH -A DIRAC-DR004-GPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=00:05:00
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:10:00
 #SBATCH --mail-type=NONE
+
+#! Do not change:
+#SBATCH -p ampere
 
 #! Number of nodes and tasks per node allocated by SLURM (do not change):
 numnodes=$SLURM_JOB_NUM_NODES
@@ -17,7 +22,7 @@ mpi_tasks_per_node=$(echo "$SLURM_TASKS_PER_NODE" | sed -e  's/^\([0-9][0-9]*\).
 #! (note that SLURM reproduces the environment at submission irrespective of ~/.bashrc):
 . /etc/profile.d/modules.sh                    # Leave this line (enables the module command)
 module purge                                   # Removes all modules still loaded
-module load rhel8/default-icl >/dev/null 2>&1  # REQUIRED - loads the basic environment
+module load rhel8/default-amp >/dev/null 2>&1  # REQUIRED - loads the basic environment
 
 #! Insert additional module load commands after this line if needed:
 source /usr/local/software/intel/oneapi/2022.1/setvars.sh >/dev/null 2>&1
@@ -33,33 +38,17 @@ workdir="$SLURM_SUBMIT_DIR"  # The value of SLURM_SUBMIT_DIR sets workdir to the
                              # in which sbatch is run.
 
 #! Are you using OpenMP (NB this is unrelated to OpenMPI)? If so increase this
-#! safe value to no more than 76:
+#! safe value to no more than 128:
 export OMP_NUM_THREADS=1
 
 #! Number of MPI tasks to be started by the application per node and in total (do not change):
 np=$[${numnodes}*${mpi_tasks_per_node}]
 
-#! The following variables define a sensible pinning strategy for Intel MPI tasks -
-#! this should be suitable for both pure MPI and hybrid MPI/OpenMP jobs:
-export I_MPI_PIN_DOMAIN=omp:compact # Domains are $OMP_NUM_THREADS cores in size
-export I_MPI_PIN_ORDER=scatter # Adjacent domains have minimal sharing of caches/sockets
-#! Notes:
-#! 1. These variables influence Intel MPI only.
-#! 2. Domains are non-overlapping sets of cores which map 1-1 to MPI tasks.
-#! 3. I_MPI_PIN_PROCESSOR_LIST is ignored if I_MPI_PIN_DOMAIN is set.
-#! 4. If MPI tasks perform better when sharing caches/sockets, try I_MPI_PIN_ORDER=compact.
-
-
-#! Uncomment one choice for CMD below (add mpirun/mpiexec options if necessary):
-
-#! Choose this for a MPI code (possibly using OpenMP) using Intel MPI.
-#CMD="mpirun -ppn $mpi_tasks_per_node -np $np $application $options"
-
 #! Choose this for a pure shared-memory OpenMP parallel program on a single node:
 #! (OMP_NUM_THREADS threads will be created):
 CMD="$application $options"
 
-#! Choose this for a MPI code (possibly using OpenMP) using OpenMPI:
+#! Choose this for a MPI code using OpenMPI:
 #CMD="mpirun -npernode $mpi_tasks_per_node -np $np $application $options"
 
 ###############################################################
@@ -92,6 +81,6 @@ mv slurm*.out $log_dir/ 2>/dev/null
 
 echo -e "\nExecuting command:\n==================\n$CMD\n"
 
-eval $CMD
+eval $CMD 
 
 mv machine.file.* $log_dir/
